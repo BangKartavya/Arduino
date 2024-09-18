31360bb25a261c724ab7e802bac3e35ef97cc888
Kartavya Bang <bangkartavya@gmail.com>
2024-09-18 20:50:21 +0530

    modified rc car

--- Arduino RC Car/src/main.cpp
+++ Arduino RC Car/src/main.cpp
@@ -1,234 +1,113 @@
-#include <Arduino.h>
+//YouTube|Tech at Home
 
-// Channel pins
+//Receiver signal pins
+double ch1_pin = 3;  //PWM pin
+double ch2_pin = 5;  //PWM pin
 
-int CH1 = 3;
-int CH2 = 5;
+//Right motor driver pins
+int R_EN_right = 2; 
+int L_EN_right = 4;
+int R_PWM_right = 10; //PWM pin
+int L_PWM_right = 11; //PWM pin
 
-// Left Motor Driver
+//Left motor driver pins
+int R_EN_left = 7; 
+int L_EN_left = 8;
+int R_PWM_left = 6; //PWM pin
+int L_PWM_left = 9; //PWM pin
 
-int L_PWM_LEFT = 10;
-int R_PWM_LEFT = 11;
+// ---**---   Rx threshold values - Update based on your TX values
+//FWD
+int Ch1Ch2_start_Fwd = 1510;
+int Ch1Ch2_End_Fwd = 2000;
+//BACK
+int Ch1Ch2_start_Bac = 1410;
+int Ch1Ch2_End_Bac = 970;
 
-// Right Motor Driver
+// ---------***---------------***----------
 
-int L_PWM_RIGHT = 6;
-int R_PWM_RIGHT = 9;
-
-// Distance Sensor
-
-int TRIG = 2;
-int ECHO = A0;
-
-// FWD
-int Ch1Ch2_start_Fwd = 1550;
-int Ch1Ch2_End_Fwd = 2030;
-// BACK
-int Ch1Ch2_start_Bac = 1450;
-int Ch1Ch2_End_Bac = 980;
-
-// Distance Threshold and Error
-
-int THRESHOLD = 20;
-int ERROR = 5;
-
-void forward(int ch1_forward_speed, int ch2_forward_speed) {
-  // spin both left and right motors clockwise
-
-  analogWrite(R_PWM_RIGHT, ch2_forward_speed);
-  analogWrite(R_PWM_LEFT, ch1_forward_speed);
-
-  analogWrite(L_PWM_RIGHT, 0);
-  analogWrite(L_PWM_LEFT, 0);
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(3,INPUT);
+  pinMode(5,INPUT);
+  pinMode(2,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(6,OUTPUT); 
+  pinMode(9,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(8,OUTPUT);
+  pinMode(10,OUTPUT); 
+  pinMode(11,OUTPUT);
 }
 
-void backward(int ch1_backward_speed, int ch2_backward_speed) {
-  // spin both motors anti clock
-  analogWrite(L_PWM_LEFT, ch2_backward_speed);
-  analogWrite(L_PWM_RIGHT, ch1_backward_speed);
+void loop()
+{
+//Receiver signal pins data
+double ch1 = pulseIn(3,HIGH);
+double ch2 = pulseIn(5,HIGH);
 
-  analogWrite(R_PWM_RIGHT, 0);
-  analogWrite(R_PWM_LEFT, 0);
+Serial.print("ch1: ");
+Serial.println(ch1);
+Serial.print("\t");
+Serial.print("ch2: ");
+Serial.print(ch2);
+Serial.println("");
+
+//Speed mapping for right motor driver
+int spdFwd_RightLeft = map(ch1, Ch1Ch2_start_Fwd, Ch1Ch2_End_Fwd, 0, 255);   
+int spdBac_RightLeft = map(ch1, Ch1Ch2_start_Bac, Ch1Ch2_End_Bac, 0, 255);    
+
+digitalWrite(R_EN_right, HIGH);
+digitalWrite(L_EN_right, HIGH);
+digitalWrite(R_EN_left, HIGH);
+digitalWrite(L_EN_left, HIGH);
+
+if((ch1==0)&&(ch2==0))
+{     
+analogWrite(R_PWM_right,0);
+analogWrite(L_PWM_right,0);
+analogWrite(R_PWM_left,0);
+analogWrite(L_PWM_left,0);
 }
-
-void left(int ch1_backward_speed) {
-  // spin LEFT motor anti clock and RIGHT motor clock;
-  analogWrite(R_PWM_RIGHT, ch1_backward_speed);
-  analogWrite(L_PWM_LEFT, ch1_backward_speed);
-
-  analogWrite(R_PWM_LEFT, 0);
-  analogWrite(L_PWM_RIGHT, 0);
+//FWD
+else if((ch1>Ch1Ch2_start_Fwd)&&(ch2>Ch1Ch2_start_Fwd))
+{     
+analogWrite(R_PWM_right,spdFwd_RightLeft);
+analogWrite(L_PWM_right,0);
+analogWrite(R_PWM_left,spdFwd_RightLeft);
+analogWrite(L_PWM_left,0); 
 }
-
-void right(int ch1_forward_speed) {
-  // spin RIGHT motor anti clock and LEFT motor clock
-  analogWrite(L_PWM_RIGHT, ch1_forward_speed);
-  analogWrite(R_PWM_LEFT, ch1_forward_speed);
-
-  analogWrite(L_PWM_LEFT, 0);
-  analogWrite(R_PWM_RIGHT, 0);
+//RIGHT
+else if((ch1>Ch1Ch2_start_Fwd)&&(ch2<Ch1Ch2_start_Bac))
+{ 
+analogWrite(R_PWM_right,0);
+analogWrite(L_PWM_right,spdFwd_RightLeft);
+analogWrite(R_PWM_left,spdFwd_RightLeft);
+analogWrite(L_PWM_left,0);
 }
-
-void forward_right(int ch1_forward_speed) {
-  // Spin both motors clockwise with LEFT motor being faster than RIGHT
-  analogWrite(R_PWM_LEFT, ch1_forward_speed);
-  analogWrite(R_PWM_RIGHT, ch1_forward_speed / 2);
-
-  analogWrite(L_PWM_RIGHT, 0);
-  analogWrite(L_PWM_LEFT, 0);
+//LEFT
+else if((ch1<Ch1Ch2_start_Bac)&&(ch2>Ch1Ch2_start_Fwd))
+{     
+analogWrite(R_PWM_right,spdBac_RightLeft);
+analogWrite(L_PWM_right,0);
+analogWrite(R_PWM_left,0);
+analogWrite(L_PWM_left,spdBac_RightLeft);
 }
-
-void forward_left(int ch1_forward_speed) {
-  // Spin both motors clockwise with LEFT motor being slower than RIGHT
-  analogWrite(R_PWM_RIGHT, ch1_forward_speed);
-  analogWrite(R_PWM_LEFT, ch1_forward_speed / 2);
-
-  analogWrite(L_PWM_RIGHT, 0);
-  analogWrite(L_PWM_LEFT, 0);
+//BACK
+else if((ch1<Ch1Ch2_start_Bac)&&(ch2<Ch1Ch2_start_Bac))
+{  
+analogWrite(R_PWM_right,0);
+analogWrite(L_PWM_right,spdBac_RightLeft); 
+analogWrite(R_PWM_left,0);
+analogWrite(L_PWM_left,spdBac_RightLeft); 
 }
-
-void backward_right(int ch1_backward_speed) {
-  // Spin both motors anticlockwise with LEFT motor being faster than RIGHT
-  analogWrite(L_PWM_LEFT, ch1_backward_speed);
-  analogWrite(L_PWM_RIGHT, ch1_backward_speed / 2);
-
-  analogWrite(R_PWM_RIGHT, 0);
-  analogWrite(R_PWM_LEFT, 0);
+else
+{     
+analogWrite(R_PWM_right,0);
+analogWrite(L_PWM_right,0);
+analogWrite(R_PWM_left,0);
+analogWrite(L_PWM_left,0);
 }
-
-void backward_left(int ch1_backward_speed) {
-  // Spin both motors anticlockwise with LEFT motor being slower than RIGHT
-  analogWrite(L_PWM_RIGHT, ch1_backward_speed);
-  analogWrite(L_PWM_LEFT, ch1_backward_speed / 2);
-
-  analogWrite(R_PWM_RIGHT, 0);
-  analogWrite(R_PWM_LEFT, 0);
-}
-
-void car_stop() {
-  // stop any motion
-  analogWrite(R_PWM_LEFT, 0);
-  analogWrite(R_PWM_RIGHT, 0);
-  analogWrite(L_PWM_LEFT, 0);
-  analogWrite(L_PWM_RIGHT, 0);
-}
-
-double ultrasonicRead(int TRIG,int ECHO) {
-  // make the trigger pin high for 10 micro seconds
-  digitalWrite(TRIG, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TRIG, LOW);
-
-  // make the echo pin high and calculate the pulse duration
-  int DUR = pulseIn(ECHO, HIGH);
-  double DIST = (0.0343 * DUR) / 2; // calculate the distance
-  return DIST;
-}
-
-void setup() {
-
-  pinMode(CH1,INPUT);
-  pinMode(CH2,INPUT);
-
-  pinMode(L_PWM_LEFT,OUTPUT);
-  pinMode(R_PWM_LEFT,OUTPUT);
-  
-  pinMode(L_PWM_RIGHT,OUTPUT);
-  pinMode(R_PWM_RIGHT,OUTPUT);
-
-  pinMode(TRIG,OUTPUT);
-  pinMode(ECHO,INPUT);
-  
-  pinMode(13,OUTPUT);
-  digitalWrite(13,LOW);
-
-  digitalWrite(TRIG,LOW);
-}
-
-
-void loop() {
-  int ch1 = pulseIn(CH1,HIGH);
-  int ch2 = pulseIn(CH2,HIGH);
-
-  // forward speeds
-
-  int ch1_forward_speed = map(ch1,Ch1Ch2_End_Fwd,Ch1Ch2_End_Fwd,0,255);
-  int ch2_forward_speed = map(ch2,Ch1Ch2_End_Fwd,Ch1Ch2_End_Fwd,0,255);
-
-  // backward speeds
-
-  int ch1_backward_speed = map(ch1,Ch1Ch2_start_Bac,Ch1Ch2_End_Bac,0,255);
-  int ch2_backward_speed = map(ch2,Ch1Ch2_start_Bac,Ch1Ch2_End_Bac,0,255);
-
-  if (ch1 == 0 || ch2 == 0) {
-    // one or both channels disconnected so stop the car
-    car_stop();
-  }
-
-  // Going Forward
-  else if ((ch1 > Ch1Ch2_start_Fwd && ch2 > Ch1Ch2_start_Fwd)) {
-    forward(ch1_forward_speed,ch2_forward_speed);
-  }
-  // Going Right
-
-  else if (ch1 > Ch1Ch2_start_Fwd && ch2 < Ch1Ch2_start_Bac) {
-    right(ch1_forward_speed);
-  }
-  // Going Left
-
-  else if (ch1 < Ch1Ch2_start_Bac && ch2 > Ch1Ch2_start_Fwd) {
-    left(ch1_backward_speed);
-  }
-
-  // Going Back
-  else if ((ch1 < Ch1Ch2_start_Bac && ch1 > Ch1Ch2_End_Bac) && ch2 < Ch1Ch2_start_Bac) {
-    backward(ch1_backward_speed,ch2_backward_speed);
-  }
-
-  // Forward Right
-  else if (ch1 > Ch1Ch2_start_Fwd && (ch2 < 1.003 * 1500 && ch2 > 0.997 * 1500)) {
-    forward_right(ch1_forward_speed);
-  }
-
-  // Forward Left
-  else if (ch2 > Ch1Ch2_start_Fwd && (ch1 < 1.003 * 1500 && ch2 > 0.997 * 1500)) {
-    forward_left(ch1_forward_speed);
-  }
-
-  // Backward Right
-  else if (ch2 < Ch1Ch2_start_Bac && (ch1 < 1.003 * 1500 && ch1 > 0.997 * 1500)) {
-    backward_right(ch1_backward_speed);
-  }
-
-  // Backward Left
-  else if ((ch1 < Ch1Ch2_start_Bac && ch1 > Ch1Ch2_End_Bac) && (ch2 < 1.003 * 1500 && ch2 > 0.997 * 1500)) {
-    backward_left(ch1_backward_speed);
-  }
-  
-  // Stick in middle
-  else if ((ch1 < 1.003 * 900 && ch1 > 0.997 * 900) && (ch1 < 1.003 * 1500 && ch1 > 0.997 * 1500)) {
-    car_stop();
-  }
-
-  else if (ch1 < 1.003 * 900 && ch1 > 0.997 * 900) {
-    // first stop the car to measure distance
-    car_stop();
-
-    double DIST = ultrasonicRead(TRIG,ECHO); // Call the ultrasonicRead function to get the distance from the left sensor
-
-    Serial.println("Distance : " + String(DIST));
-
-    if(DIST == 0.00) car_stop();
-    else if(DIST > THRESHOLD - ERROR && DIST < THRESHOLD + ERROR) car_stop();
-    else if(DIST > THRESHOLD + ERROR) backward(25,25);
-    else if(DIST < THRESHOLD - ERROR) forward(25,25);
-    else car_stop();
- 
-  }
-  
-  else {
-    // unexpected thing happened
-    car_stop();
-  }
 
 }
--- blinker/blinker.ino
+++ blinker/blinker.ino
@@ -1,10 +1,236 @@
-void setup() {
-    pinMode(13,OUTPUT);
+// Channel pins
+
+int CH1 = 3;
+int CH2 = 5;
+
+// Left Motor Driver
+
+int L_PWM_LEFT = 10;
+int R_PWM_LEFT = 11;
+
+// Right Motor Driver
+
+int L_PWM_RIGHT = 6;
+int R_PWM_RIGHT = 9;
+
+// Distance Sensor
+
+int TRIG = 2;
+int ECHO = A0;
+
+// FWD
+int Ch1Ch2_start_Fwd = 1550;
+int Ch1Ch2_End_Fwd = 2030;
+// BACK
+int Ch1Ch2_start_Bac = 1450;
+int Ch1Ch2_End_Bac = 980;
+
+// Distance Threshold and Error
+
+int THRESHOLD = 20;
+int ERROR = 5;
+
+void forward(int ch1_forward_speed, int ch2_forward_speed) {
+  // spin both left and right motors clockwise
+
+  analogWrite(R_PWM_RIGHT, ch2_forward_speed);
+  analogWrite(R_PWM_LEFT, ch1_forward_speed);
+
+  analogWrite(L_PWM_RIGHT, 0);
+  analogWrite(L_PWM_LEFT, 0);
 }
 
+void backward(int ch1_backward_speed, int ch2_backward_speed) {
+  // spin both motors anti clock
+  analogWrite(L_PWM_LEFT, ch2_backward_speed);
+  analogWrite(L_PWM_RIGHT, ch1_backward_speed);
+
+  analogWrite(R_PWM_RIGHT, 0);
+  analogWrite(R_PWM_LEFT, 0);
+}
+
+void left(int ch1_backward_speed) {
+  // spin LEFT motor anti clock and RIGHT motor clock;
+  analogWrite(R_PWM_RIGHT, ch1_backward_speed);
+  analogWrite(L_PWM_LEFT, ch1_backward_speed);
+
+  analogWrite(R_PWM_LEFT, 0);
+  analogWrite(L_PWM_RIGHT, 0);
+}
+
+void right(int ch1_forward_speed) {
+  // spin RIGHT motor anti clock and LEFT motor clock
+  analogWrite(L_PWM_RIGHT, ch1_forward_speed);
+  analogWrite(R_PWM_LEFT, ch1_forward_speed);
+
+  analogWrite(L_PWM_LEFT, 0);
+  analogWrite(R_PWM_RIGHT, 0);
+}
+
+void forward_right(int ch1_forward_speed) {
+  // Spin both motors clockwise with LEFT motor being faster than RIGHT
+  analogWrite(R_PWM_LEFT, ch1_forward_speed);
+  analogWrite(R_PWM_RIGHT, ch1_forward_speed / 2);
+
+  analogWrite(L_PWM_RIGHT, 0);
+  analogWrite(L_PWM_LEFT, 0);
+}
+
+void forward_left(int ch1_forward_speed) {
+  // Spin both motors clockwise with LEFT motor being slower than RIGHT
+  analogWrite(R_PWM_RIGHT, ch1_forward_speed);
+  analogWrite(R_PWM_LEFT, ch1_forward_speed / 2);
+
+  analogWrite(L_PWM_RIGHT, 0);
+  analogWrite(L_PWM_LEFT, 0);
+}
+
+void backward_right(int ch1_backward_speed) {
+  // Spin both motors anticlockwise with LEFT motor being faster than RIGHT
+  analogWrite(L_PWM_LEFT, ch1_backward_speed);
+  analogWrite(L_PWM_RIGHT, ch1_backward_speed / 2);
+
+  analogWrite(R_PWM_RIGHT, 0);
+  analogWrite(R_PWM_LEFT, 0);
+}
+
+void backward_left(int ch1_backward_speed) {
+  // Spin both motors anticlockwise with LEFT motor being slower than RIGHT
+  analogWrite(L_PWM_RIGHT, ch1_backward_speed);
+  analogWrite(L_PWM_LEFT, ch1_backward_speed / 2);
+
+  analogWrite(R_PWM_RIGHT, 0);
+  analogWrite(R_PWM_LEFT, 0);
+}
+
+void car_stop() {
+  // stop any motion
+  analogWrite(R_PWM_LEFT, 0);
+  analogWrite(R_PWM_RIGHT, 0);
+  analogWrite(L_PWM_LEFT, 0);
+  analogWrite(L_PWM_RIGHT, 0);
+}
+
+double ultrasonicRead(int TRIG,int ECHO) {
+  // make the trigger pin high for 10 micro seconds
+  digitalWrite(TRIG, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG, LOW);
+
+  // make the echo pin high and calculate the pulse duration
+  int DUR = pulseIn(ECHO, HIGH);
+  double DIST = (0.0343 * DUR) / 2; // calculate the distance
+  return DIST;
+}
+
+void setup() {
+
+  pinMode(CH1,INPUT);
+  pinMode(CH2,INPUT);
+
+  pinMode(L_PWM_LEFT,OUTPUT);
+  pinMode(R_PWM_LEFT,OUTPUT);
+  
+  pinMode(L_PWM_RIGHT,OUTPUT);
+  pinMode(R_PWM_RIGHT,OUTPUT);
+
+  Serial.begin(9600);
+
+  pinMode(TRIG,OUTPUT);
+  pinMode(ECHO,INPUT);
+  
+  pinMode(13,OUTPUT);
+  digitalWrite(13,LOW);
+
+  digitalWrite(TRIG,LOW);
+}
+
+
 void loop() {
-    digitalWrite(13,HIGH);
-    delay(1000);
-    digitalWrite(13,LOW);
-    delay(1000);
+  int ch1 = pulseIn(CH1,HIGH);
+  int ch2 = pulseIn(CH2,HIGH);
+
+//  Serial.println("CH1 : " + String(ch1) + " CH2 : " + String(ch2));
+
+  // forward speeds
+
+  int ch1_forward_speed = map(ch1,Ch1Ch2_End_Fwd,Ch1Ch2_End_Fwd,0,255);
+  int ch2_forward_speed = map(ch2,Ch1Ch2_End_Fwd,Ch1Ch2_End_Fwd,0,255);
+
+  // backward speeds
+
+  int ch1_backward_speed = map(ch1,Ch1Ch2_start_Bac,Ch1Ch2_End_Bac,0,255);
+  int ch2_backward_speed = map(ch2,Ch1Ch2_start_Bac,Ch1Ch2_End_Bac,0,255);
+
+  if (ch1 == 0 || ch2 == 0) {
+    // one or both channels disconnected so stop the car
+    car_stop();
+  }
+
+  // Going Forward
+  else if ((ch1 > Ch1Ch2_start_Fwd && ch2 > Ch1Ch2_start_Fwd)) {
+    forward(ch1_forward_speed,ch2_forward_speed);
+  }
+  // Going Right
+
+  else if (ch1 > Ch1Ch2_start_Fwd && ch2 < Ch1Ch2_start_Bac) {
+    right(ch1_forward_speed);
+  }
+  // Going Left
+
+  else if (ch1 < Ch1Ch2_start_Bac && ch2 > Ch1Ch2_start_Fwd) {
+    left(ch1_backward_speed);
+  }
+
+  // Going Back
+  else if ((ch1 < Ch1Ch2_start_Bac && ch1 > Ch1Ch2_End_Bac) && ch2 < Ch1Ch2_start_Bac) {
+    backward(ch1_backward_speed,ch2_backward_speed);
+  }
+
+  // Forward Right
+  else if (ch1 > Ch1Ch2_start_Fwd && (ch2 < 1.003 * 1500 && ch2 > 0.997 * 1500)) {
+    forward_right(ch1_forward_speed);
+  }
+
+  // Forward Left
+  else if (ch2 > Ch1Ch2_start_Fwd && (ch1 < 1.003 * 1500 && ch2 > 0.997 * 1500)) {
+    forward_left(ch1_forward_speed);
+  }
+
+  // Backward Right
+  else if (ch2 < Ch1Ch2_start_Bac && (ch1 < 1.003 * 1500 && ch1 > 0.997 * 1500)) {
+    backward_right(ch1_backward_speed);
+  }
+
+  // Backward Left
+  else if ((ch1 < Ch1Ch2_start_Bac && ch1 > Ch1Ch2_End_Bac) && (ch2 < 1.003 * 1500 && ch2 > 0.997 * 1500)) {
+    backward_left(ch1_backward_speed);
+  }
+  
+  // Stick in middle
+  else if ((ch1 < 1.003 * 900 && ch1 > 0.997 * 900) && (ch1 < 1.003 * 1500 && ch1 > 0.997 * 1500)) {
+    car_stop();
+  }
+
+  else if (ch1 < 1.003 * 900 && ch1 > 0.997 * 900) {
+    // first stop the car to measure distance
+    car_stop();
+
+    double DIST = ultrasonicRead(TRIG,ECHO); // Call the ultrasonicRead function to get the distance from the left sensor
+
+    Serial.println("Distance : " + String(DIST));
+
+    if(DIST == 0.00) car_stop();
+    else if(DIST > THRESHOLD - ERROR && DIST < THRESHOLD + ERROR) car_stop();
+    else if(DIST > THRESHOLD + ERROR) backward(25,25);
+    else if(DIST < THRESHOLD - ERROR) forward(25,25);
+    else car_stop();
+ 
+  }
+  
+  else {
+    // unexpected thing happened
+    car_stop();
+  }
+
 }
